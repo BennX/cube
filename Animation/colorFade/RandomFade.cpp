6459c9a680d8ad877729f4dd3a2ab2a94c603a1e
BennX <bennimeyer@web.de>
2015-09-06 15:10:05 +0200

    Changed the effect so that the color...
    Just get interpolated between the two points and not behind of them seen
    from the other point.

--- Animation/colorFade/RandomFade.cpp
+++ Animation/colorFade/RandomFade.cpp
@@ -71,12 +71,50 @@ void RandomFade::update(const uint16_t& delta)
             {
                 for(int8_t z = 0; z < 5; z++)
                 {
-                    float distanceToP0 = sqrt(pow((float)x - m_p0.x, 2) + pow((float)y - m_p0.y, 2) + pow((float)z - m_p0.z, 2));
-                    float distanceToP1 = sqrt(pow((float)x - m_p1.x, 2) + pow((float)y - m_p1.y, 2) + pow((float)z - m_p1.z, 2));
-                    //float relative =sqrt(pow((float)m_p1.x-m_p0.x,2)+pow((float)m_p1.y-m_p0.y,2)+pow((float)m_p1.z-m_p0.z,2));
-                    float relative = distanceToP0 + distanceToP1;
-                    RGB dotCol = m_color0 * (1.0f - (distanceToP0 / relative)) + m_color1 * (1.0f - (distanceToP1 / relative));
-                    Cube::getInstance().setRGB(x, y, z, dotCol);
+                    //check if this point is behind the other seen from p0 oder p1
+                    // and colorize it just by that color
+                    // if its between these 2 points
+                    //colorize it depending on the distance
+
+                    //calculate the vectors between the points
+                    Vector p0p1(m_p1 - m_p0);
+                    Vector p1p0(m_p0 - m_p1);
+                    //calculate the normal of the "imagin" plane
+                    Vector planNormalp0p1(p0p1);
+                    planNormalp0p1 *= -1;
+                    planNormalp0p1.nor();
+                    Vector planNormalp1p0(p1p0);
+                    planNormalp1p0 *= -1;
+                    planNormalp1p0.nor();
+
+                    //calc the vector to the point we check
+                    Vector vectorToPos0(Vector(x, y, z) - m_p0);
+                    Vector vectorToPos1(Vector(x, y, z) - m_p1);
+
+                    //dot it with the normals of the "planes"
+                    float dotPos0Planp0p1 = vectorToPos0.dot(planNormalp0p1);
+                    float dotPos1Planp1p0 = vectorToPos1.dot(planNormalp1p0);
+
+                    if(dotPos0Planp0p1 < 0 && dotPos1Planp1p0 > 0)
+                    {
+                        //behind p1 seen from p0!
+                        Cube::getInstance().setRGB(x, y, z, m_color1);
+                    }
+                    else if(dotPos1Planp1p0 < 0 && dotPos0Planp0p1 > 0)
+                    {
+                        //behind p0 seen from p1
+                        Cube::getInstance().setRGB(x, y, z, m_color0);
+                    }
+                    else
+                    {
+                        //between
+                        float distanceToP0 = sqrt(pow((float)x - m_p0.x, 2) + pow((float)y - m_p0.y, 2) + pow((float)z - m_p0.z, 2));
+                        float distanceToP1 = sqrt(pow((float)x - m_p1.x, 2) + pow((float)y - m_p1.y, 2) + pow((float)z - m_p1.z, 2));
+                        //float relative =sqrt(pow((float)m_p1.x-m_p0.x,2)+pow((float)m_p1.y-m_p0.y,2)+pow((float)m_p1.z-m_p0.z,2));
+                        float relative = distanceToP0 + distanceToP1;
+                        RGB dotCol = m_color0 * (1.0f - (distanceToP0 / relative)) + m_color1 * (1.0f - (distanceToP1 / relative));
+                        Cube::getInstance().setRGB(x, y, z, dotCol);
+                    }
                 }
             }
         }
